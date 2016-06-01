645bc12fa00769a5604fc7d6fb7baf0edf1f06b8
nbahulek <nbahulek@umich.edu>
2016-06-01 12:21:10 -0400

    Fix src/org/uofm/ot/fusekiAccessLayer/ folder
    Former-commit-id: 5bef21b03bb012149661b1ed4ab7f7c157875a0e

--- src/org/uofm/ot/fusekiAccessLayer/FusekiConstants.java
+++ src/org/uofm/ot/fusekiAccessLayer/FusekiConstants.java
@@ -8,6 +8,14 @@ public class FusekiConstants {
 	
 	public static final String PREFIX_OT = "PREFIX ot: <http://uofm.org/objectteller/> ";
 	
+	public static final String PREFIX_PROV = "PREFIX prov: <http://www.w3.org/ns/prov#> ";
 	
+	public static final String FEDORA_NAMESPACE =  "http://fedora.info/definitions/v4/repository#";
+	
+	public static final String OT_NAMESPACE =  "http://uofm.org/objectteller/";
+	
+	public static final String DC_NAMESPACE = "http://purl.org/dc/elements/1.1/";
+	
+	public static final String PROV_NAMESPACE = "http://www.w3.org/ns/prov#";
 	
 }
--- src/org/uofm/ot/fusekiAccessLayer/FusekiService.java
+++ src/org/uofm/ot/fusekiAccessLayer/FusekiService.java
@@ -9,11 +9,9 @@ import java.util.Date;
 import java.util.Locale;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
@@ -21,11 +19,14 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.log4j.Logger;
 import org.uofm.ot.dao.SystemConfigurationDAO;
 import org.uofm.ot.exception.ObjectTellerException;
+import org.uofm.ot.fedoraAccessLayer.ChildType;
 import org.uofm.ot.fedoraAccessLayer.FedoraObject;
+import org.uofm.ot.fedoraAccessLayer.PayloadDescriptor;
 import org.uofm.ot.model.Server_details;
-import org.apache.log4j.Logger;
 
 public class FusekiService {
 	
@@ -86,8 +87,7 @@ public class FusekiService {
 			} 
 		} catch (ConnectException ex){
 			logger.error("Check if fuseki server up and running. ");
-			ObjectTellerException exception = new ObjectTellerException(ex);
-			// #TODO User error : Check if fuseki server up and running. 
+			ObjectTellerException exception = new ObjectTellerException(ex); 
 			throw exception;
 		}
 
@@ -127,7 +127,6 @@ public class FusekiService {
 		} catch (ConnectException ex){
 			logger.error("Check if fuseki server up and running. ");
 			ObjectTellerException exception = new ObjectTellerException(ex);
-			// #TODO User error : Check if fuseki server up and running. 
 			throw exception;
 		}
 
@@ -176,7 +175,11 @@ public class FusekiService {
 			QuerySolution binding = resultSet.nextSolution();
 
 			FedoraObject fedoraObject = new FedoraObject();
-			fedoraObject.setURI(binding.get("x").toString());
+			String uri = binding.get("x").toString();
+			uri = uri.substring(fedoraServerURL.length());
+			fedoraObject.setURI(uri);
+			
+			
 			if(binding.get("published") != null) {
 				if("YES".equals(binding.get("published").toString().toUpperCase()))
 					fedoraObject.setPublished(true);
@@ -189,23 +192,11 @@ public class FusekiService {
 			}
 			
 			fedoraObject.setTitle(binding.get("title").toString());
-			String createdOn = binding.get("created").toString();
-			String lastModified = binding.get("lastModified").toString();
-			lastModified = lastModified.substring(0, 10);
-			createdOn = createdOn.substring(0,10);
-			try {
-				Date dateLastModified = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(lastModified);
-				fedoraObject.setLastModified(dateLastModified);
-				
-				Date datecreatedOn = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(createdOn);
-				fedoraObject.setCreatedOn(datecreatedOn);
-				
-			} catch (ParseException e) {
-				ObjectTellerException exception = new ObjectTellerException(e);
-				exception.setErrormessage("Unable to parse created on date or last updated date");
-				throw exception;
-			}
-			
+			Date createdOn = convertRDFNodetoDate(binding.get("created"));
+			Date lastModified = convertRDFNodetoDate(binding.get("lastModified"));
+			fedoraObject.setLastModified(lastModified);
+			fedoraObject.setCreatedOn(createdOn);
+
 			list.add(fedoraObject);
 
 		}
@@ -266,49 +257,177 @@ public class FusekiService {
 			if ( 200 == httpResponse.getStatusLine().getStatusCode())
 				result = true;
 		} catch (ClientProtocolException e) {
+			logger.error("Not able to connect to the Fuseki with url "+fusekiURL);
 			ObjectTellerException exception = new ObjectTellerException(e);
 			exception.setErrormessage("Not able to connect to the Fuseki with url "+fusekiURL);
 			throw exception;
 		} catch (IOException e) {
 			ObjectTellerException exception = new ObjectTellerException(e);
 			exception.setErrormessage("Not able to connect to the Fuseki with url "+fusekiURL);
+			logger.error("Not able to connect to the Fuseki with url "+fusekiURL);
 			throw exception;
 		}
 		
 		return result;
 	}
 	
-	/*public FedoraObject getObjectProperties(FedoraObject fedoraObject) throws ObjectTellerException {
+	public FedoraObject getObjectProperties(FedoraObject fedoraObject) throws ObjectTellerException {
 		if(fusekiServerURL != null ) {
 			if(testIfFusekiIsRunning()) {
-			String queryString = FusekiConstants.PREFIX_OT+"\n"+
 
-			 "SELECT  ?p ?o \n"+
+				String uri = fedoraServerURL+fedoraObject.getURI();
+				String queryString = FusekiConstants.PREFIX_OT+"\n"+
 
-			 "WHERE {  \n"+
-			 "<"+fedoraObject.getURI()+">  ?p ?o.\n"+ 
-			 "} \n" ;
+				 "SELECT  ?p ?o \n"+
 
-			Query query = QueryFactory.create(queryString) ;
-			QueryExecution execution = QueryExecutionFactory.sparqlService(fusekiServerURL, query);
-			ResultSet resultSet = execution.execSelect();
+				 "WHERE {  \n"+
+				 "<"+uri+">  ?p ?o.\n"+ 
+				 "} \n" ;
 
-			if (resultSet.hasNext()) {
-				QuerySolution binding = resultSet.nextSolution();
-				String countString = binding.get("count").toString();
+				Query query = QueryFactory.create(queryString) ;
+				QueryExecution execution = QueryExecutionFactory.sparqlService(fusekiServerURL, query);
+				ResultSet resultSet = execution.execSelect();
 
+				while (resultSet.hasNext()) {
+					QuerySolution binding = resultSet.nextSolution();
+					String predicate = binding.get("p").toString();
+					if(predicate.contains(FusekiConstants.DC_NAMESPACE) == true) {
+						String actualPredicate = predicate.substring(FusekiConstants.DC_NAMESPACE.length());
+						if("title".equals(actualPredicate)){
+							fedoraObject.setTitle(binding.get("o").toString());
+						}
+					} else {
+						if(predicate.contains(FusekiConstants.FEDORA_NAMESPACE) == true){
+							String actualPredicate = predicate.substring(FusekiConstants.FEDORA_NAMESPACE.length());
+							if("lastModified".equals(actualPredicate)  )
+								fedoraObject.setLastModified(convertRDFNodetoDate(binding.get("o")));
 
-				String[] array =  countString.split("\\^\\^");
-				if(array.length > 0) {
-					count = Integer.valueOf(array[0]);
+							if( "created".equals(actualPredicate))
+								fedoraObject.setCreatedOn(convertRDFNodetoDate(binding.get("o")));
+
+						} else {
+							if(predicate.contains(FusekiConstants.OT_NAMESPACE) == true) {
+								String actualPredicate = predicate.substring(FusekiConstants.OT_NAMESPACE.length());
+								if("keywords".equals(actualPredicate) )
+									fedoraObject.setKeywords(binding.get("o").toString());
+
+								if( "owner".equals(actualPredicate)) 
+									fedoraObject.setOwner(binding.get("o").toString());
+
+								if( "contributors".equals(actualPredicate) )
+									fedoraObject.setContributors(binding.get("o").toString());
+
+								if( "description".equals(actualPredicate))
+									fedoraObject.setDescription(binding.get("o").toString());
+
+								if( "published".equals(actualPredicate)) {
+									if("YES".equals(binding.get("o").toString().toUpperCase()))
+										fedoraObject.setPublished(true);
+									else
+										fedoraObject.setPublished(false);
+								}
+							}
+						}
+					}
 				}
-			}
 			}
 		} else {
 			logger.error("Fuseki Server URL is not configured");
 			ObjectTellerException exception = new ObjectTellerException("Fuseki Server URL is not configured");
 			throw exception;
 		} 
-		return null;
-	} */
+		return fedoraObject;
+	} 
+	
+	private Date convertRDFNodetoDate(RDFNode o) throws ObjectTellerException{
+		Date convertedDate = null;
+		try {
+			String date = o.toString();
+			date = date.substring(0, 10);
+			convertedDate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(date);
+		} catch (ParseException e) {
+			logger.error("Unable to parse created on date or last updated date" + e.getCause());
+			ObjectTellerException exception = new ObjectTellerException(e);
+			exception.setErrormessage("Unable to parse created on date or last updated date" + e.getCause());
+			throw exception;
+		}
+		return convertedDate;
+	}
+	
+	public PayloadDescriptor getPayloadProperties( String objectURI) throws ObjectTellerException {
+		PayloadDescriptor descriptor = null;
+		if(fusekiServerURL != null ) {
+			if(testIfFusekiIsRunning()) {
+
+				String uri = fedoraServerURL+objectURI+"/"+ChildType.PAYLOAD.getChildType();
+				String queryString = FusekiConstants.PREFIX_OT+"\n"+
+
+				 "SELECT  ?p ?o \n"+
+
+				 "WHERE {  \n"+
+				 "<"+uri+">  ?p ?o.\n"+ 
+				 "} \n" ;
+
+				Query query = QueryFactory.create(queryString) ;
+				QueryExecution execution = QueryExecutionFactory.sparqlService(fusekiServerURL, query);
+				ResultSet resultSet = execution.execSelect();
+
+				descriptor = new PayloadDescriptor();
+				
+				while (resultSet.hasNext()) {
+					QuerySolution binding = resultSet.nextSolution();
+					String predicate = binding.get("p").toString();
+					if(predicate.contains(FusekiConstants.OT_NAMESPACE) == true) {
+						String actualPredicate = predicate.substring(FusekiConstants.OT_NAMESPACE.length());
+						if("functionName".equals(actualPredicate) )
+							descriptor.setFunctionName(binding.get("o").toString());
+
+						if( "executorType".equals(actualPredicate)) 
+							descriptor.setEngineType(binding.get("o").toString());
+					}
+				}
+			}
+		} else {
+			logger.error("Fuseki Server URL is not configured");
+			ObjectTellerException exception = new ObjectTellerException("Fuseki Server URL is not configured");
+			throw exception;
+		} 
+		return descriptor;
+	} 
+	
+	public String getObjectProvProperties(String objectURI) throws ObjectTellerException {
+		String logData = "";
+		if(fusekiServerURL != null ) {
+			if(testIfFusekiIsRunning()) {
+
+				String uri = fedoraServerURL+objectURI;
+				String queryString = FusekiConstants.PREFIX_OT+"\n"+
+
+				 "SELECT  ?p ?o \n"+
+
+				 "WHERE {  \n"+
+				 "<"+uri+">  ?p ?o.\n"+ 
+				 "} \n" ;
+
+				Query query = QueryFactory.create(queryString) ;
+				QueryExecution execution = QueryExecutionFactory.sparqlService(fusekiServerURL, query);
+				ResultSet resultSet = execution.execSelect();
+
+				while (resultSet.hasNext()) {
+					QuerySolution binding = resultSet.nextSolution();
+					String predicate = binding.get("p").toString();
+					if(predicate.contains(FusekiConstants.PROV_NAMESPACE) == true) {
+						String actualPredicate = predicate.substring(FusekiConstants.PROV_NAMESPACE.length());
+						logData = logData + objectURI + " " + actualPredicate + " " + binding.get("o").toString() +" \n ";
+					}
+				}
+			}
+		} else {
+			logger.error("Fuseki Server URL is not configured");
+			ObjectTellerException exception = new ObjectTellerException("Fuseki Server URL is not configured");
+			throw exception;
+		} 
+		return logData;
+	}
+
 }
