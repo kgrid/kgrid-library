3b3720f9ed2944f58e86b559b0cbb52ebda560e7
pboisver <pboisver@msiss-MacBook-Pro-2.local>
2016-08-01 10:30:45 -0400

    initial mavenize commit
    Former-commit-id: 0185ba1b6b740dcb7b394007ef1fed80e6ed0ee6

--- ObjectTellerDB_server_details.sql
+++ ObjectTellerDB_server_details.sql
@@ -1,6 +1,6 @@
 UPDATE ObjectTellerDB.server_details
 SET setting_type = 'FEDORA_SERVER', ip_address = '10.16.6.32', svr_name = 'dlhs-fedora-dev-a.umms.med.umich.edu',
-  svr_username   = 'fedoraAdmin', svr_passwd = 'secret321', svr_port = 8080,
+  svr_username   = 'fedoraAdmin', svr_passwd = '****', svr_port = 8080,
   complete_url   = 'http://dlhs-fedora-dev-a.umms.med.umich.edu:8080/fcrepo/rest/'
 WHERE id = 1;
 UPDATE ObjectTellerDB.server_details
