62ba7760b0c6476a9390fea0ccfd7c6fea328242
nbahulek <nbahulek@med.umich.edu>
2016-08-10 12:29:01 -0400

    Removed BASE-Namespace hard-coding

--- src/main/java/org/uofm/ot/adapter/OWLAdapter.java
+++ src/main/java/org/uofm/ot/adapter/OWLAdapter.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.io.OWLObjectRenderer;
 import org.semanticweb.owlapi.model.OWLAxiom;
@@ -34,6 +35,9 @@ import uk.ac.manchester.cs.owlapi.dlsyntax.DLSyntaxObjectRenderer;
 
 public class OWLAdapter {
 	
+	private static final Logger logger = Logger.getLogger(OWLAdapter.class);
+	
+	
 	public Result execute(String input, String OWLpayload) throws ObjectTellerException {
 		Result result = new Result();
 		
@@ -41,21 +45,15 @@ public class OWLAdapter {
 		
 		Map<String, List<String>> opInferredMap =  (Map<String, List<String>>) new HashMap<String, List<String>>();
 
-	//	final String BASE_IND_NS = "http://www.semanticweb.org/nbahulek/ontologies/2016/6/untitled-ontology-20";
-
-		String BASE_IND_NS = "http://med.umich.edu/cci/ontologies/domain-jnc7-hypertension-model-plus-individuals";
-
 		OWLOntologyManager managerWC = OWLManager.createOWLOntologyManager();
 
 		InputStream streamPayload = new ByteArrayInputStream(OWLpayload.getBytes(StandardCharsets.UTF_8));
 
-		OWLOntology ontologyWC;
+		OWLOntology payloadOntology;
 		try {
-			ontologyWC = managerWC.loadOntologyFromOntologyDocument(streamPayload);
+			payloadOntology = managerWC.loadOntologyFromOntologyDocument(streamPayload);
 
-			//System.out.println("***************** "+ontologyWC.getOntologyID().getDefaultDocumentIRI().toString());
-			
-		//	String BASE_IND_NS = ontologyWC.getOntologyID().getDefaultDocumentIRI().toString();
+			String BASE_IND_NS = payloadOntology.getOntologyID().getDefaultDocumentIRI().toString();
 			
 			OWLOntologyManager ptManager = OWLManager.createOWLOntologyManager();
 
@@ -63,18 +61,14 @@ public class OWLAdapter {
 
 			OWLOntology ptOntology = ptManager.loadOntologyFromOntologyDocument(streamInput);
 
-		//	System.out.println("Axiom count before "+ontologyWC.getAxiomCount());
-
-
 			Set<OWLAxiom> axioms = ptOntology.getAxioms();
 
 			for (OWLAxiom owlAxiom : axioms) {
-				managerWC.addAxiom(ontologyWC, owlAxiom);
+				managerWC.addAxiom(payloadOntology, owlAxiom);
 			}
 
-		//	System.out.println("Axiom count after "+ontologyWC.getAxiomCount());
 			OWLDataFactory factoryWC = managerWC.getOWLDataFactory();
-			PrefixOWLOntologyFormat pmWC = (PrefixOWLOntologyFormat) managerWC.getOntologyFormat(ontologyWC);
+			PrefixOWLOntologyFormat pmWC = (PrefixOWLOntologyFormat) managerWC.getOntologyFormat(payloadOntology);
 			pmWC.setDefaultPrefix(BASE_IND_NS + "#");
 			//get a given individual
 
@@ -82,39 +76,28 @@ public class OWLAdapter {
 			OWLNamedIndividual personRDF = factoryWC.getOWLNamedIndividual(":"+person, pmWC);
 			
 			OWLReasonerFactory reasonerFactoryWC = PelletReasonerFactory.getInstance();
-			OWLReasoner reasonerWC = reasonerFactoryWC.createReasoner(ontologyWC, new SimpleConfiguration());
+			OWLReasoner reasonerWC = reasonerFactoryWC.createReasoner(payloadOntology, new SimpleConfiguration());
 			
 			OWLObjectRenderer renderer = new DLSyntaxObjectRenderer();
 			
-			Set<OWLClassExpression> assertedClasses = personRDF.getTypes(ontologyWC);
-			
-			
-			
-			List<OWLClass> inferredOWLClasses = new ArrayList<OWLClass>();
-
-			
-			String resultString = "";
+			Set<OWLClassExpression> assertedClasses = personRDF.getTypes(payloadOntology);
+		
 			for (OWLClass c : reasonerWC.getTypes(personRDF, true).getFlattened()) {
-				
 
 				boolean asserted = assertedClasses.contains(c);
 				if(!asserted ) {
-					if( ! inferredOWLClasses.contains(c))
-						inferredOWLClasses.add(c);
-				}
-				for (OWLClass cls : reasonerWC.getSuperClasses(c, false).getFlattened() ) {
-					boolean asserted1 = assertedClasses.contains(cls);
-					if(!asserted1) {
-						if(inferredMap.get(renderer.render(cls)) == null ){
-							ArrayList<String> list = new ArrayList<String>();
-							list.add(renderer.render(c));
-							inferredMap.put(renderer.render(cls),list);
-						} else {
-							inferredMap.get(renderer.render(cls)).add(renderer.render(c));
+					for (OWLClass cls : reasonerWC.getSuperClasses(c, false).getFlattened() ) {
+						boolean asserted1 = assertedClasses.contains(cls);
+						if(!asserted1) {
+							if(inferredMap.get(renderer.render(cls)) == null ) {
+								ArrayList<String> list = new ArrayList<String>();
+								list.add(renderer.render(c));
+								inferredMap.put(renderer.render(cls),list);
+							} else {
+								inferredMap.get(renderer.render(cls)).add(renderer.render(c));
+							}
 						}
-
 					}
-					
 				}
 			}
 			
@@ -132,8 +115,7 @@ public class OWLAdapter {
 			result.setSuccess(1);
 
 		} catch (OWLOntologyCreationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();		
+			logger.error("An error occured while executing OWL reasoner "+ e );
 			result.setSuccess(0);
 			result.setErrorMessage(e.getMessage());
 			throw new ObjectTellerException(e);
