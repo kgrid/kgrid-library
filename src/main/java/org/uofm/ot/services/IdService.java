0657d3802eb70ed93efe7905a25270733ce48467
nbahulek <nbahulek@umich.edu>
2016-09-19 16:23:26 -0400

    Adding modify,bind,status method in EzidService and corresponding tests. Adding properties file.
    Former-commit-id: f52806397f906ab7cacbc241f3485b587ac8444b

--- src/main/java/org/uofm/ot/services/IdService.java
+++ src/main/java/org/uofm/ot/services/IdService.java
@@ -1,15 +1,24 @@
 package org.uofm.ot.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by pboisver on 9/15/16.
  */
+@Service
 public class IdService {
+	
+	@Autowired
+	private EzidService ezidService;
 
-    static int id = 0;
-    static String naan = "12345";
-
-    public String mint() {
-        String pid = String.valueOf(id++);
-        return "ark:/" + naan + "/" + pid;
+	public String mint() {
+        return ezidService.mint();
     }
+
+	public void setEzidService(EzidService ezidService) {
+		this.ezidService = ezidService;
+	}
+    
+    
 }
--- src/test/java/org/uofm/ot/services/IdServiceTest.java
+++ src/test/java/org/uofm/ot/services/IdServiceTest.java
@@ -1,51 +1,61 @@
 package org.uofm.ot.services;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
-
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/ObjectTellerServlet-servlet.xml" })
+@PropertySource("classpath:application.properties")
 public class IdServiceTest {
+	
+	@Autowired
+	private IdService idService;
+	
+	@Before
+	public void setUp() {
+		
+	}
 
-    private IdService idService;
+	@After
+	public void tearDown() throws Exception {
 
-    @Before
-    public void setUp() {
+	}
 
-    idService = new IdService();
+	
+	@Test
+	public void canMintTwoNewArkIdsThatAreDifferent() {
+		
+	
+		
+		String arkId = idService.mint();
 
-}
+		String arkId2 = idService.mint();
 
-    @After
-    public void tearDown() throws Exception {
+		assertNotEquals("Should be different", arkId, arkId2);
 
-    }
+	}
 
-    @Test
-    public void canMintTwoNewArkIdsThatAreDifferent() {
+	@Test
+	public void arkIdsStartWithStringArkColonAndHave3Parts() {
 
-        String arkId = idService.mint();
-
-        String arkId2 = idService.mint();
-
-        assertNotEquals("Should be different", arkId, arkId2);
-
-    }
-
-    @Test
-    public void arkIdsStartWithStringArkColonAndHave3Parts() {
-
-        String arkId = idService.mint();
+		String arkId = idService.mint();
 
 
-        String[] idParts = arkId.split("/");
+		String[] idParts = arkId.split("/");
 
-        assertEquals(3, idParts.length);
+		assertEquals(3, idParts.length);
 
-        assertEquals("Should be 'ark:'", "ark:", idParts[0]);
-    }
+		assertEquals("Should be 'ark:'", "ark:", idParts[0]);
+	}
 
 
 }
