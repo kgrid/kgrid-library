65fd078ae7c36521a1f92c7c64940bcf6382f01b
George Meng <gqmeng@umich.edu>
2016-10-13 12:24:27 -0400

    Move vue-based Html and related resources to object-teller;

---
+++ src/main/webapp/WEB-INF/vuehome/js/ot.js
@@ -0,0 +1,150 @@
+'use strict';
+function retrieveObjectList(fillObjectList){
+		$.ajax({
+			type : "GET",
+			url : "/ObjectTeller/knowledgeObject",
+			xhrFields: {
+			     withCredentials: true
+			},
+			success : function(response, tStatus, xhr) {
+				console.log(response);
+				fillObjectList(response);
+			},
+			error : function(response) {
+				errorHandler(response);
+			}
+		});
+}
+
+function retrieveObject(uri, section, fillObjectContent){
+	var endPoint = uri; 
+	if(section!=""){
+		endPoint = endPoint+ "/"+section;
+	}
+	console.log("Endpoint:"+endPoint);
+	$.ajax({
+		type : "GET",
+		url : "/ObjectTeller/knowledgeObject/"+endPoint,
+		xhrFields: {
+		     withCredentials: true
+		},
+		success : function(response, tStatus, xhr) {
+			fillObjectContent(response);
+		},
+		error : function(response) {
+			errorHandler(response);
+		}
+	});
+}
+
+function loadFieldsConfig(fillFieldsConfig){
+	$.getJSON("json/fields.json", fillFieldsConfig);
+}
+
+function login() {
+	
+	var user = new Object();
+	user.username = document.getElementById("username").value;
+	user.passwd = document.getElementById("password").value;
+
+	var text = JSON.stringify(user);
+	console.log(text);
+	
+	var validForm=login_validator.form();
+	if(validForm){
+		console.log("validation result: " +validForm);
+		$( "div.processing" ).fadeIn( 300 );
+		$.ajax({
+				beforeSend : function(xhrObj) {
+					xhrObj.setRequestHeader("Content-Type",
+							"application/json");
+					xhrObj.setRequestHeader("Accept", "application/json");
+				},
+				type : 'POST',
+				url : "/ObjectTeller/login",
+				data : text,
+				dataType : "json",
+
+				success : function(response) {
+				 if(response!='empty') {
+						  var test = JSON.stringify(response);
+					      var obj = JSON.parse(test);
+					      location.reload();
+				    }
+				} ,
+				
+				error : function(response) {
+					// TODO: Handle Error Message
+					
+					$( "div.processing" ).fadeOut( 200 );
+					 $( "div.failure" ).fadeIn( 300 ).delay( 1500 ).fadeOut( 400 );
+				}
+			});
+	}else{
+		}
+	
+	}
+
+function autoresize() {
+	var eid= $(this).attr("id");
+	var sh = $(this)[0].scrollHeight;
+	$(this).css("height","0px");     //Reset height, so that it not only grows but also shrinks
+	$(this).css('height',sh+ 'px');    //Set new height
+}
+
+function backToTop() {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+}
+
+function setBannerbkSize(){
+  		var vp_width=$(window).width();
+  		if(vp_width>1584){
+  			$("#bannerbk").css("width",vp_width+"px");
+  		}
+  	}
+ 
+ 	
+$(document).ready(function() {
+	/*    var link = document.querySelector('link[rel="import"]');
+	    var content = link.import;
+	    // Grab DOM from warning.html's document.
+	    var el = content.querySelector('.ot-nav');
+	    document.body.appendChild(el.cloneNode(true));
+	*/
+	    $('[data-toggle="tooltip"]').tooltip();
+		$(window).resize(function(){setBannerbkSize()});
+	 	setBannerbkSize();
+	    $('.autosize').each(autoresize);
+	    $('ul#tabs li:first').addClass('active'); 
+	    $('ul#tab li:first').addClass('active'); 
+		var padLeft = 280;
+	    var navOffset = $(".header").offset().top;
+	    $(".header").wrap('<div class="theadwrapper"></div>');
+		$(".theadwrapper").height($(".header").outerHeight(false));
+
+	    $(window).scroll(function () {
+	    	var supportPageOffset = window.pageXOffset !== undefined;
+	    	var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+	 	    var x = supportPageOffset ? window.pageXOffset : isCSS1Compat ? document.documentElement.scrollLeft : document.body.scrollLeft;
+	    	var y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+	            var scrollPos = $(window).scrollTop();
+	        if(scrollPos>60){
+	        	$(".ot-backtotop").fadeIn('slow');
+	        	$("#backtotop").fadeIn('slow');
+	        }else{
+	        	$(".ot-backtotop").fadeOut('slow');
+	        	$("#backtotop").fadeOut('slow');
+	        }
+	       if (scrollPos >= navOffset) {
+	            $(".header").addClass("fixed");
+	        } else {
+	            $(".header").removeClass("fixed");
+	        }
+	       $(".fixed").css("left",-x+"px");
+	    });
+	    
+
+})
