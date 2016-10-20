07b8cff48678d83f93e15fa784601b3a1c55123b
nbahulek <nbahulek@umich.edu>
2016-10-20 10:59:05 -0400

    Renamed FedoraObjectTest to KnowledgeObjectTest.
    Former-commit-id: 56986df77cda987a603e23cba79cdb9360aaabe7

--- src/test/java/org/uofm/ot/knowledgeObject/KnowledgeObjectTest.java
+++ src/test/java/org/uofm/ot/knowledgeObject/KnowledgeObjectTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
-public class FedoraObjectTest {
+public class KnowledgeObjectTest {
 
 	@Test
 	public void fedoraObjectHasArkId () {
