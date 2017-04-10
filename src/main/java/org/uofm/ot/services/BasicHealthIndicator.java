5791d363c9d6aed0634a97be85b9653544e46c89
pboisver <pboisver@umich.edu>
2017-04-10 15:58:40 -0400

    Test code example for externalized properties (fedora, etc.)
    Former-commit-id: 88a7e0baedc270e4942bcab74c60e686a2789275

---
+++ src/main/java/org/uofm/ot/services/BasicHealthIndicator.java
@@ -0,0 +1,27 @@
+package org.uofm.ot.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BasicHealthIndicator extends AbstractHealthIndicator {
+
+    @Autowired
+    ConfigService configService;
+
+    @Value("${library.name:ObjectTeller3000}")
+    String libraryName;
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        builder
+            .withDetail("library.name", libraryName)
+            .withDetail("fedora.name", configService.getName())
+            .up()
+//            .withException(new IndexOutOfBoundsException("Uh-oh!"))
+        ;
+    }
+}
