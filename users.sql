3eaa06a3a67a7567bb6e43d6931c454a05d5a8cd
pboisver <pboisver@umich.edu>
2017-06-29 16:54:22 -0400

    initial commit Add getting started info to README Split database script
    Former-commit-id: 7e11f2c64bc81f0c773752813549806c275f71ba

--- users.sql
+++ users.sql
@@ -2,19 +2,13 @@
 This script creates the minimal standard spring security user tables, plus
 a user_profile table for any extra user information.
 
-The script migrates users in the existing user table (USER_DETAILS) to the new schema and
-performs a simple test for migration success. There is an optional section that can
-be configured to create test users.
-
-The script DOES NOT remove the original data
-
 The script is intended to be run manually. See inline comments for details.
 */
 
 
 -- set to your schema if different
 
-USE ObjectTellerDB;
+USE library;
 
 -- Uncomment to allow script to be rerun, drop tables in this order to avoid fk constraints
 
@@ -51,67 +45,7 @@ CREATE TABLE user_profiles
     ON DELETE CASCADE ON UPDATE CASCADE 
 );
 
--- Migrate exist data from user_details
-
-INSERT INTO users (username, password)
-  SELECT
-    username,
-    passwd
-  FROM USER_DETAILS;
-
-INSERT INTO authorities (username, authority)
-  SELECT
-    username,
-    role
-  FROM USER_DETAILS;
-
-INSERT INTO user_profiles (username, first_name, last_name)
-  SELECT
-    username,
-    first_name,
-    last_name
-  FROM USER_DETAILS;
-
--- Create view like USER_DETAILS for testing
-
-DROP VIEW IF EXISTS vw_user_details;
-
-CREATE VIEW vw_user_details AS
-  SELECT
-
-    u.username,
-    u.password  passwd,
-    a.authority role,
-    p.first_name,
-    p.last_name
-  FROM users u, authorities a, user_profiles p
-  WHERE u.username = a.username AND u.username = p.username;
-
--- Compare the view and
-
-
-SELECT
-  username,
-  passwd,
-  role,
-  first_name,
-  last_name
-FROM USER_DETAILS
-WHERE username NOT IN (SELECT username
-                       FROM vw_user_details)
-UNION
-SELECT
-  username,
-  passwd,
-  role,
-  first_name,
-  last_name
-FROM vw_user_details
-WHERE username NOT IN (SELECT username
-                       FROM USER_DETAILS);
-
 -- Uncomment to add test users (may conflict with migrated user w/ same username)
-
 
   INSERT INTO users (username, password)
   VALUES
@@ -120,13 +54,13 @@ WHERE username NOT IN (SELECT username
 
   INSERT INTO authorities (username, authority)
   VALUES
-    ('admin@umich.edu', 'ADMIN'),
-    ('user@umich.edu', 'USER');
+    ('admin@kgrid.org', 'ADMIN'),
+    ('user@kgrid.org', 'USER');
 
   INSERT INTO user_profiles (username, first_name, last_name)
   VALUES
-    ('user@umich.edu', 'fuser', 'luser'),
-    ('admin@umich.edu', 'fadmin', 'ladmin');
+    ('user@kgrid.org', 'Ulrich', 'User'),
+    ('admin@kgrid.org', 'Alvin', 'Admin');
 
   commit;
 
