f4140b2f105a9375a9955e678db9805f269f4ffd
George Meng <gqmeng@umich.edu>
2017-07-12 14:09:41 -0400

    webpack proxy fix; add kgrid-default.css
    Former-commit-id: aa84b42da4336ee526bff480f0ecfb131ccf2dae

--- config/index.js
+++ config/index.js
@@ -4,6 +4,68 @@ var path = require('path')
 // allow command line override of connected library path, e.g.
 // $ LIBRARY=http://localhost:8080/ObjectTeller npm run dev
 var library = process.env.LIBRARY || 'http://localhost:8080'
+var splitRegExp = new RegExp(
+        '^' +
+            '(?:' +
+            '([^:/?#.]+)' +                         // scheme - ignore special characters
+                                                    // used by other URL parts such as :,
+                                                    // ?, /, #, and .
+            ':)?' +
+            '(?://' +
+            '(?:([^/?#]*)@)?' +                     // userInfo
+            '([\\w\\d\\-\\u0100-\\uffff.%]*)' +     // domain - restrict to letters,
+                                                    // digits, dashes, dots, percent
+                                                    // escapes, and unicode characters.
+            '(?::([0-9]+))?' +                      // port
+            ')?' +
+            '([^?#]+)?' +                           // path
+            '(?:\\?([^#]*))?' +                     // query
+            '(?:#(.*))?' +                          // fragment
+            '$');
+var split = library.match(splitRegExp)
+var splitpath = split[5] || ''
+var ppath = splitpath
+// console.log(split);
+// console.log(ppath);
+var ops={
+    target: library,
+    secure: false,
+    changeOrigin: true,
+    onProxyRes(proxyRes, req, res) {
+      // proxyRes.headers['Set-Cookie']=proxyRes.headers['set-cookie'];
+      var existingCookies = proxyRes.headers['set-cookie'];
+      var rewrittenCookies = [];
+      if (existingCookies !== undefined) {
+        if (!Array.isArray(existingCookies)) {
+          existingCookies = [existingCookies];
+        }
+        for (let i = 0; i < existingCookies.length; i++) {
+          var re = /ath=(.*);/;
+          var match=existingCookies[i].match(re);
+          if(match!=null){
+              rewrittenCookies.push(existingCookies[i].replace(match[1], "/"));
+          }else{
+            rewrittenCookies.push(existingCookies[i]);
+          }
+
+        }
+        proxyRes.headers['set-cookie'] = rewrittenCookies;
+      }
+      Object.keys(proxyRes.headers).forEach(function (key) {
+        var newkey = key.replace(/((?:^|-)[a-z])/g, function(val) { return val.toUpperCase(); });
+        // custom hack for X-Parse-Os-Version ==> X-Parse-OS-Version
+        newkey = newkey.replace(/(-Os-)/g, function(val) { return val.toUpperCase(); });
+        proxyRes.headers[newkey] = proxyRes.headers[key];
+
+        res.append(newkey, proxyRes.headers[newkey]);
+      });
+    },
+    onProxyReq(proxyReq, req, res) {
+      Object.keys(req.headers).forEach(function (key) {
+        proxyReq.setHeader(key, req.headers[key]);
+      });
+    }
+  }
 
 module.exports = {
   build: {
@@ -29,26 +91,15 @@ module.exports = {
     env: require('./dev.env'),
     port: 3000,
     autoOpenBrowser: true,
+    contentBase: path.join(__dirname, "dist"),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-      // proxy each backend api route separately, rewrites not needed
-	proxyTable: {
-        '/user': {
-            target: library,
-            changeOrigin: true,
-        },
-        '/knowledgeObject': {
-            target: library,
-            changeOrigin: true,
-        },
-        '/login': {
-            target: library,
-            changeOrigin: true,
-        },
-        '/logout': {
-            target: library,
-            changeOrigin: true,
-        }
+    //    proxy each backend api route separately, rewrites not needed
+	  proxyTable: {
+        '/user':ops,
+        '/login':ops,
+        '/logout':ops,
+        '/knowledgeObject':ops
     },
 	// CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
