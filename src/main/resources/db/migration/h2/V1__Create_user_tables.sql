c560e0d5513f8addfc9a55d1981d9e95821f1766
pboisver <pboisver@umich.edu>
2017-07-25 15:26:58 -0400

    h2/flyway setup

---
+++ src/main/resources/db/migration/h2/V1__Create_user_tables.sql
@@ -0,0 +1,68 @@
+/*
+This script creates the minimal standard spring security user tables, plus
+a user_profile table for any extra user information.
+
+The script is intended to be run manually. See inline comments for details.
+*/
+
+
+-- set to your schema if different
+
+-- CREATE SCHEMA IF NOT EXISTS library2;
+--
+-- SET SCHEMA library2; -- database specific
+
+-- Uncomment to allow script to be rerun, drop tables in this order to avoid fk constraints
+
+DROP TABLE IF EXISTS authorities, user_profiles, users;
+
+-- Create user schema
+
+CREATE TABLE users
+(
+  username VARCHAR(50) PRIMARY KEY NOT NULL,
+  password VARCHAR(50)             NOT NULL,
+  enabled  BOOLEAN                 NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE authorities
+(
+  username  VARCHAR(50) NOT NULL PRIMARY KEY,
+  authority VARCHAR(50) NOT NULL,
+  CONSTRAINT fk_authorities_users
+  FOREIGN KEY (username)
+  REFERENCES users (username)
+  ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE user_profiles
+(
+  id         IDENTITY PRIMARY KEY,
+  username   VARCHAR(50)           NOT NULL,
+  first_name VARCHAR(50)           NOT NULL,
+  last_name  VARCHAR(50)           NOT NULL,
+  CONSTRAINT fk_user_profiles_users
+  FOREIGN KEY (username)
+  REFERENCES users (username)
+  ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Uncomment to add test users (may conflict with migrated user w/ same username)
+
+  INSERT INTO users (username, password)
+  VALUES
+    ('user@kgrid.org', 'user123'),
+    ('admin@kgrid.org', 'admin123');
+
+  INSERT INTO authorities (username, authority)
+  VALUES
+    ('admin@kgrid.org', 'ADMIN'),
+    ('user@kgrid.org', 'USER');
+
+  INSERT INTO user_profiles (username, first_name, last_name)
+  VALUES
+    ('user@kgrid.org', 'Ulrich', 'User'),
+    ('admin@kgrid.org', 'Alvin', 'Admin');
+
+  commit;
+
