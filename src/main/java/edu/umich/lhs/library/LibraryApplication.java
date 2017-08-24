ca8a659592abf35172ae908290dd8bc1189dd0cb
pboisver <pboisver@umich.edu>
2017-08-24 17:00:27 -0400

    add build and git info to actuator info endpoint; move library.name from fcrepo health check to info
    Former-commit-id: dfd9dda0adb0c2588c19ae326915fc2138227fb4

--- src/main/java/edu/umich/lhs/library/LibraryApplication.java
+++ src/main/java/edu/umich/lhs/library/LibraryApplication.java
@@ -1,13 +1,20 @@
 package edu.umich.lhs.library;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.SimpleInfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class LibraryApplication extends WebMvcConfigurerAdapter {
+
+    public LibraryApplication(@Value("${library.name}") String name) {
+        this.name = name;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
@@ -19,4 +26,16 @@ public class LibraryApplication extends WebMvcConfigurerAdapter {
         registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
         super.addViewControllers( registry );
     }
+
+    @Bean
+    SimpleInfoContributor getLibraryNameContributor()  {
+        return new SimpleInfoContributor("library.name", name);
+    }
+
+    private String name;
+
+    public String getLibraryName() {
+        return name;
+    }
+
 }
