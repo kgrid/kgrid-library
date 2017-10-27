759734db914a68304ba3198970f365807a2f08f9
George Meng <gqmeng@umich.edu>
2017-10-27 12:39:59 -0400

    Added Bootstrap css/js/fonts, Jquery, Google Fonts to run locally.

--- src/main/vuejs/main.js
+++ src/main/vuejs/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VeeValidate from 'vee-validate';
 import moment from 'moment'
-
 import Vuex from 'vuex';
 import App from './App';
 import store from './store';
