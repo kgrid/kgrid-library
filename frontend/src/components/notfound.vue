5f3f55c495f7654a15a90acfba538231e7392eda
George Meng <gqmeng@umich.edu>
2018-05-31 17:13:58 -0400

    Initial Commit for multi-module
    Former-commit-id: 03fb3e90fba061188395d338f4c6e4064a45aa17

---
+++ frontend/src/components/notfound.vue
@@ -0,0 +1,46 @@
+<template id='notfound'>
+<div class='content'>
+	<applayout :nothelper='false'>
+		<div slot='banner'>
+			<p> About Banner </p>
+		</div>
+		<div slot='header'>
+			<p> About Header </p>
+		</div>
+		<div slot='maincontent'>
+		<div id="about_grid" class="infogrid" >
+			<div class="infoblock">
+				<div class="block_title">
+					<div class="line">
+						<hr></div>Error 404<div class="line"><hr></div>
+					</div>
+				<div class="block_info">The page you are looking for is not here.</div>
+			</div>
+		</div></div>
+		</applayout>
+</div>
+</template>
+<script>
+import applayout from './applayout.vue';
+export default {
+    name: 'notfound',
+	data : function() {
+		return {
+		}
+	},
+	created : function() {
+	},
+	mounted:function(){
+	},
+	computed : {
+	},
+	methods : {
+	},
+	components:{
+		'applayout':applayout
+		}
+};
+</script>
+<style>
+
+</style>
---
+++ frontend/src/components/objcreator.vue
@@ -0,0 +1,170 @@
+<template>
+	<olpane layerid=0 :stage='stage'>
+		<div slot="oltitle"><h3>Add a Knowledge Object to the Library </h3></div>
+		<div slot="ol-form" >
+				<div class="panel-body">
+					<div id="file-drag-drop">
+						<form ref="fileform" v-show='files.length==0'>
+							<span class="drop-files">Drop the KO ZIP file here!</span>
+						</form>
+						<div v-for="(file, key) in files" class="file-listing" v-show='files.length>0'>
+							{{ file.name }}
+							<div class="inline float-r">
+								<a class="remove " v-on:click="removeFile( key )"><i class='fa fa-times'></i></a>
+							</div>
+						</div>
+						<progress max="100" :value.prop="uploadPercentage" v-show='files.length>0'></progress>
+					</div>
+				</div>
+			</div>
+			<div slot='buttons'>
+					<button class="kg-btn-primary" v-bind:disabled='files.length == 0' v-on:click="submitFiles">Deposit Object</button>
+			</div>
+		<div slot="ol-processing"><span>Processing...</span></div>
+		<div slot="ol-success"><span>The knowledge Object succesfully added to the shelf.  Object ID:{{arkid}}</span></div>
+		<div slot="ol-failure"><span>Error in depositing the KO! </span></div>
+		<div slot="ol-warning"><span>Warning !!!</span></div>
+	</olpane>
+</template>
+	<script>
+	import olpane from '../components/olpane';
+	export default {
+		name:"objcreator",
+		data:function(){
+			return {
+				dragAndDropCapable: false,
+				files: [],
+				uploadPercentage: 0,
+				stage:''
+			}
+		},
+	created:function(){},
+	components: {
+		olpane
+		},
+	computed:{
+		arkid:function(){
+			if(this.files.length>0){
+				var filename = this.files[0].name
+				var index=filename.lastIndexOf('/')
+				var l= filename.length
+				var s = filename.slice(index+1,l)
+				 return 'ark:/'+s.replace('.zip','').replace('-','/')
+			 }else {
+				 return ''
+			 }
+		},
+		url:function(){
+			return this.$store.getters.getshelfurl
+		}
+	},
+	mounted:function(){
+		/*			Determine if drag and drop functionality is capable in the browser		*/
+		this.dragAndDropCapable = this.determineDragAndDropCapable();
+		/*			If drag and drop capable, then we continue to bind events to our elements.		*/
+		if( this.dragAndDropCapable ){
+			/*
+				Listen to all of the drag events and bind an event listener to each
+				for the fileform.
+			*/
+			['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach( function( evt ) {
+				/*
+					For each event add an event listener that prevents the default action
+					(opening the file in the browser) and stop the propagation of the event (so
+					no other elements open the file in the browser)
+				*/
+				this.$refs.fileform.addEventListener(evt, function(e){
+					e.preventDefault();
+					e.stopPropagation();
+				}.bind(this), false);
+			}.bind(this));
+			/*
+				Add an event listener for drop to the form
+			*/
+			this.$refs.fileform.addEventListener('drop', function(e){
+				/*
+					Capture the files from the drop event and add them to our local files
+					array.
+				*/
+				for( let i = 0; i < e.dataTransfer.files.length; i++ ){
+					this.files.push( e.dataTransfer.files[i] );
+				}
+			}.bind(this));
+		}
+	},
+	methods:{
+		determineDragAndDropCapable(){
+			var div = document.createElement('div');
+			return ( ( 'draggable' in div )
+							|| ( 'ondragstart' in div && 'ondrop' in div ) )
+							&& 'FormData' in window
+							&& 'FileReader' in window;
+		},
+		/*			Submits the files to the server		*/
+		submitFiles(){
+			var formData = new FormData();
+			var file = this.files[0];
+			var self = this;
+			formData.append('ko', file);
+			this.stage='processing';
+			setTimeout(function(){
+			self.$http.put( self.url+self.arkid,
+				formData,
+				{
+					onUploadProgress: function( progressEvent ) {
+						this.uploadPercentage = parseInt( Math.round( ( progressEvent.loaded * 100 ) / progressEvent.total ) );
+					}.bind(this)
+				}
+			).then(function(resp){
+				self.stage='success'
+				setTimeout(function(){
+					console.log(resp);
+
+					self.$eventBus.$emit('objSaved',{})
+				} , 1500)
+			})
+			.catch(function(err){
+				self.stage='error'
+				console.log(err);
+			});}, 1000)
+		},
+		removeFile( key ){
+			this.files.splice( key, 1 );
+		},
+	}
+};
+	</script>
+	<style scoped>
+	form{
+		display: block;
+		height: 400px;
+		width: 400px;
+		background: #ccc;
+		margin: auto;
+		margin-top: 40px;
+		text-align: center;
+		line-height: 400px;
+		border-radius: 4px;
+	}
+	div.file-listing{
+		width: 400px;
+		margin: auto;
+		padding: 10px;
+		border-bottom: 1px solid #ddd;
+	}
+	.alert-box  div span{
+		color: #fff;
+	}
+	progress{
+		width: 400px;
+		margin: auto;
+		display: block;
+		margin-top: 20px;
+		margin-bottom: 20px;
+	}
+	.panel-body {
+		height:630px;
+		padding: 15px;
+		border:1px solid #0075bc;
+	}
+	</style>
