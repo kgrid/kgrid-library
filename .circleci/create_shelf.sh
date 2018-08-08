6e98e741026b0d752bf397f7d4d3661e7d04aa32
Greg Farris <greg.t.farris@gmail.com>
2018-08-08 15:46:09 -0400

    Adding sample set of KOs to heroku library on deploy(wrong shelf local)

--- .circleci/create_shelf.sh
+++ .circleci/create_shelf.sh
@@ -5,7 +5,7 @@
  .circleci/download_assets.sh https://api.github.com/repos/kgrid/mopen-opioid-collection/releases/latest
  .circleci/download_assets.sh https://api.github.com/repos/kgrid/kgrid-activator/releases/latest
  .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/cancer-risk/releases/latest
- unzip -o opioid-all.zip -d shelf
- unzip -o cpic-all.zip -d shelf
- unzip -o hello-world.zip -d shelf
- unzip -o cancer-risk.zip -d shelf
+ unzip -o opioid-all.zip -d application/shelf
+ unzip -o cpic-all.zip -d application/shelf
+ unzip -o hello-world.zip -d application/shelf
+ unzip -o cancer-risk.zip -d application/shelf
