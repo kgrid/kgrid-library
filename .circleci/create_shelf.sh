4df4b54c197cc218e9f9c0ce61e238a61fab6886
George Meng <gqmeng.meng@gmail.com>
2018-08-21 15:42:29 -0400

    Added postpci assets to create_shelf.sh

--- .circleci/create_shelf.sh
+++ .circleci/create_shelf.sh
@@ -7,9 +7,11 @@
  .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/cancer-risk/releases/latest
  .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/icon-array/releases/latest
  .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/script-numerate/releases/latest
+ .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/postpci/releases/latest
  unzip -o opioid-all.zip -d application/shelf
  unzip -o cpic-all.zip -d application/shelf
  unzip -o hello-world.zip -d application/shelf
  unzip -o cancer-risk.zip -d application/shelf
  unzip -o icon-array.zip -d application/shelf
  unzip -o scriptnumerate-all.zip -d application/shelf
+ unzip -o postpci-all.zip -d application/shelf
