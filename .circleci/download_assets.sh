db9b6431aab05707e33e964fe488ee7b521e5c3b
gregfarris <farrisg@umich.edu>
2019-04-09 15:00:53 -0400

    #165185065 Altered deployment to use new ko import via manifest

--- .circleci/download_assets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-artifacts=($(curl -s $1 | jq -r ".assets[].browser_download_url"))
-echo ${#artifacts[@]} assests downloading
-
-for url in "${artifacts[@]}"
-do
-	curl -L -O $url
-done
