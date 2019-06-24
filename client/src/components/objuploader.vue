6fd06632dedafc6b3930fb8d9361a09f9524fe9f
George Meng <gqmeng.meng@gmail.com>
2019-06-24 16:09:08 -0400

    Updated objuploader.vue to allow the front end handle multiple-file upload;

--- client/src/components/objuploader.vue
+++ client/src/components/objuploader.vue
@@ -2,8 +2,8 @@
 	<olpane layerid=0 :stage='stage'>
 		<div slot="oltitle"><h3>Add a Knowledge Object to the Library </h3></div>
 		<div slot="ol-form" >
-				<div style='margin-top:250px;'>
-					<FilePond ref='fpond' name='ko' allowMultiple instantUpload=false />
+				<div style='margin-top:220px;'>
+					<FilePond ref='fpond' name='ko' allowMultiple instantUpload=false allowRevert=false />
 				</div>
 			</div>
 			<div slot='buttons'>
@@ -19,7 +19,7 @@
 				 </v-btn>
 			</div>
 		<div slot="ol-processing"><span>Processing...</span></div>
-		<div slot="ol-success"><span>The Knowledge Object has been succesfully added to the shelf.  Object ID: {{arkid}}</span></div>
+		<div slot="ol-success"><span>{{filecount}} Knowledge <span v-if='filecount>1'>Objects have</span> <span v-else>Object has</span> been succesfully added to the shelf. </span></div>
 		<div slot="ol-failure"><span>Error in depositing the KO! </span></div>
 		<div slot="ol-warning"><span>Warning !!!</span></div>
 	</olpane>
@@ -33,7 +33,8 @@
 		data:function(){
 			return {
 				stage:'',
-				arkid:''
+				requestField:'ko',
+				filecount:0
 			}
 		},
 	components: {
@@ -43,58 +44,89 @@
 	computed:{
 		url:function(){
 			return this.$store.getters.getbaseurl
-		}
+		},
+
 	},
 	methods:{
 		uploadFiles () {
-			var self=this
+			var self = this
+			var files = this.$refs.fpond.getFiles()
 			setOptions( {
 				server: {
 					url: self.url,
 					process: (fieldName, file, metadata, load, error, progress, abort) => {
-					    // set data
-					    const formData = new FormData();
-					    formData.append(fieldName, file, file.name);
-					    // related to aborting the request
-					    const CancelToken = self.$http.CancelToken;
-					    const source = CancelToken.source();
-					    // the request itself
-					    self.$http({
-					        method: 'post',
-					        url: self.url,
-					        data: formData,
-					        cancelToken: source.token,
-					        onUploadProgress: (e) => {
-					            progress(e.lengthComputable, e.loaded, e.total);
-					        }
-					    }).then(response => {
-									self.stage='success'
-									self.arkid = response.data.Added
-									setTimeout(function(){
-										console.log(response);
-										self.$eventBus.$emit('objAdded',{})
-									} , 1500)
+							// set data
+							const formData = new FormData();
+							formData.append(self.requestField, file, file.name);
+							// related to aborting the request
+							const CancelToken = self.$http.CancelToken;
+							const source = CancelToken.source();
+							// the request itself
+							self.$http({
+									method: 'post',
+									url: self.url,
+									data: formData,
+									cancelToken: source.token,
+									onUploadProgress: (e) => {
+											progress(e.lengthComputable, e.loaded, e.total);
+									}
+							}).then(response => {
+					        // passing the file id to FilePond
+					        load(file.name)
 					    }).catch((thrown) => {
 					        if (self.$http.isCancel(thrown)) {
 					            console.log('Request canceled', thrown.message);
 					        } else {
 					            // handle error
-											self.stage='error'
-											console.log(thrown);
 					        }
 					    });
-					    // Setup abort interface
-					    return {
-					        abort: () => {
-					            source.cancel('Operation canceled by the user.');
-					        }
-					    };
+							// Setup abort interface
+							return {
+									abort: () => {
+											source.cancel('Operation canceled by the user.');
+									}
+							};
 					}
 				}
 			} )
-			self.stage='processing';
-			this.$refs.fpond.processFile()
-		}
+			var findex = files.map(function(e, index){return index})
+			console.log(findex)
+			this.filecount = files.length
+			this.uploadFile(findex)
+			.then(
+				()=>{
+					console.log('Done with depositing '+files.length+ ' KOs.')
+					self.stage='success'
+					setTimeout(function(){
+						self.$eventBus.$emit('objAdded',{})
+					} , 1200)
+				}, (err) => {
+					self.stage='error'
+					console.log(err)
+				}
+			)
+		},
+		uploadFile: function(array) {
+			var self = this
+			var index = 0
+ 			return new Promise(function(resolve,reject){
+				function next() {
+					console.log(index)
+					if (index < array.length) {
+						self.$refs.fpond.processFile(array[index++])
+						.then(function(file){
+							console.log(index+" done")
+							next()
+						}).catch(function(error){
+							reject()
+						});
+					} else {
+						resolve();
+					}
+				}
+				next();
+			})
+		},
 	}
 };
 	</script>
