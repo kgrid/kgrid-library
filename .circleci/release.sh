2241d191abbdb976f04390907adc3bdefdb1e7dd
George Meng <gqmeng.meng@gmail.com>
2020-02-18 14:38:39 -0500

    Updated config.yml and release.sh to address the deprecation of the query param in GITHUB API.

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -10,16 +10,12 @@
                           -Darguments="-Ddockerfile.tag=$TAG_NAME" release:perform -P ci -s .circleci/settings.xml
 
  # Create GitHub Release capture release id
- RELEASE_ID=$(curl -X POST "https://api.github.com/repos/kgrid/kgrid-library/releases?access_token=$GITHUB_TOKEN" \
-                         -H 'Content-Type:application/json' -d "{\"tag_name\":\"$TAG_NAME\",  \"name\":\"KGrid Library $TAG_NAME\", \"prerelease\":$PRERELEASE}" | jq -r ".id")
+ RELEASE_ID=$(curl -X POST "https://api.github.com/repos/kgrid/kgrid-library/releases" \
+                        -H "Authorization: token $GITHUB_TOKEN" -H 'Content-Type:application/json' -d "{\"tag_name\":\"$TAG_NAME\",  \"name\":\"KGrid Library $TAG_NAME\", \"prerelease\":$PRERELEASE}" | jq -r ".id")
 
  echo Release $RELEASE_ID created
 
  #Upload jar file to GitHub Release
- curl -X POST "https://uploads.github.com/repos/kgrid/kgrid-library/releases/$RELEASE_ID/assets?access_token=$GITHUB_TOKEN&name=$FILE_NAME" \
-           -H "Content-Type: application/octet-stream" \
+ curl -X POST "https://uploads.github.com/repos/kgrid/kgrid-library/releases/$RELEASE_ID/assets?name=$FILE_NAME" \
+           -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/octet-stream" \
            --data-binary "@application/target/$FILE_NAME"
-
-
-
-
