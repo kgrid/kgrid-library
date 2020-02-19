2baf5e65fbd3c8f7a1ce10fad755dd105cb3530c
George Meng <gqmeng.meng@gmail.com>
2020-02-19 10:24:03 -0500

    Updated create_shelf.sh

--- .circleci/create_shelf.sh
+++ .circleci/create_shelf.sh
@@ -10,9 +10,9 @@
      if [[ $i == *:* ]]; then
        theReop=$(echo $i | cut -d':' -f 1)
        theTag=$(echo $i | cut -d':' -f 2)
-       url=(https://api.github.com/repos/kgrid-objects/$theReop/releases/tags/$theTag?access_token=$GIT_TOKEN )
+       url=(https://api.github.com/repos/kgrid-objects/$theReop/releases/tags/$theTag )
      else
-       url=(https://api.github.com/repos/kgrid-objects/$i/releases/latest?access_token=$GIT_TOKEN  )
+       url=(https://api.github.com/repos/kgrid-objects/$i/releases/latest  )
      fi
 
      assets=($(curl -s $url | jq -r ".assets[].browser_download_url"))
