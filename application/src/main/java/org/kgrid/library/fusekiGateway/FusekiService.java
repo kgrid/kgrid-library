d32b8cbac741b06611cb92b6b6622c8c34a9385c
ngittlen <nggittle@med.umich.edu>
2020-09-23 10:31:25 -0400

    Updated to latest shelf version

--- application/src/main/java/org/kgrid/library/fusekiGateway/FusekiService.java
+++ application/src/main/java/org/kgrid/library/fusekiGateway/FusekiService.java
@@ -1,13 +1,16 @@
 package org.kgrid.library.fusekiGateway;
 
-import org.kgrid.library.model.ServerDetails;
 import org.apache.jena.arq.querybuilder.SelectBuilder;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.sparql.engine.http.QueryExceptionHTTP;
 import org.apache.log4j.Logger;
-import org.kgrid.library.services.FedoraConfiguration;
 import org.kgrid.library.exception.LibraryException;
+import org.kgrid.library.model.ServerDetails;
+import org.kgrid.library.services.FedoraConfiguration;
+import org.kgrid.shelf.domain.KnowledgeObjectWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.net.ConnectException;
 import java.text.ParseException;
@@ -15,129 +18,133 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
-import org.kgrid.shelf.domain.KnowledgeObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 @Component
 public class FusekiService {
 
-	private FedoraConfiguration fedoraConfiguration;
-	
-	private String fusekiServerURL;
-	
-	private static final Logger logger = Logger.getLogger(FusekiService.class);
+  private FedoraConfiguration fedoraConfiguration;
 
-	public FusekiService() {}
+  private String fusekiServerURL;
 
-	@Autowired
-	public FusekiService(FedoraConfiguration fedoraConfiguration) {
-		setFedoraConfiguration(fedoraConfiguration);
-	}
-		
-	private void setFedoraConfiguration(FedoraConfiguration fedoraConfiguration) {
-		this.fedoraConfiguration = fedoraConfiguration;
-		initFusekiUrl();
-	}
-	
-	private void initFusekiUrl(){
-		ServerDetails fusekiServer = fedoraConfiguration.getFusekiServerConfiguration();
-		if(fusekiServer != null){
-			fusekiServerURL = fusekiServer.getUrl();
-		}
-	}
+  private static final Logger logger = Logger.getLogger(FusekiService.class);
 
-	public ArrayList<KnowledgeObject> getKnowledgeObjects(boolean published) throws LibraryException {
-		try {
-			if(fedoraConfiguration.getFusekiServerConfiguration().getUrl() != null ) {
-				Query query = initQuery(published);
+  public FusekiService() {}
 
-				return getFedoraObjects(query);
-			} else {
-				logger.error("Fuseki Server URL is not configured");
-				throw new LibraryException("Fuseki Server URL is not configured");
-			} 
-		} catch (ConnectException ex){
-			logger.error("Check if fuseki server up and running. " + ex);
-			throw new LibraryException(ex);
-		}
-	}
+  @Autowired
+  public FusekiService(FedoraConfiguration fedoraConfiguration) {
+    setFedoraConfiguration(fedoraConfiguration);
+  }
 
-	public int countUniqueArkIds() throws LibraryException {
-		try {
-			SelectBuilder countArkIds = new SelectBuilder()
-					.addVar("count(*)", "?count")
-					.addWhere("?s", "<http://uofm.org/objectteller/arkId>", "?o");
-			QueryExecution queryExecution = QueryExecutionFactory.sparqlService(fusekiServerURL, countArkIds.build());
-			ResultSet resultSet = queryExecution.execSelect();
-			return resultSet.next().get("count").asLiteral().getInt();
-		} catch (org.apache.jena.sparql.lang.sparql_11.ParseException e) {
-			throw new LibraryException("Cannot create query for count of ark ids" + e);
-		}
-	}
+  private void setFedoraConfiguration(FedoraConfiguration fedoraConfiguration) {
+    this.fedoraConfiguration = fedoraConfiguration;
+    initFusekiUrl();
+  }
 
-	private Query initQuery(boolean published) {
+  private void initFusekiUrl() {
+    ServerDetails fusekiServer = fedoraConfiguration.getFusekiServerConfiguration();
+    if (fusekiServer != null) {
+      fusekiServerURL = fusekiServer.getUrl();
+    }
+  }
 
-		String subject = "?x";
+  public ArrayList<KnowledgeObjectWrapper> getKnowledgeObjects(boolean published)
+      throws LibraryException {
+    try {
+      if (fedoraConfiguration.getFusekiServerConfiguration().getUrl() != null) {
+        Query query = initQuery(published);
 
-		SelectBuilder getAllObjects = new SelectBuilder()
-				.addPrefix(NamespaceConstants.OT_NAMESPACE_PREFIX, NamespaceConstants.OT_NAMESPACE_URL)
-				.addPrefix(NamespaceConstants.DC_NAMESPACE_PREFIX, NamespaceConstants.DC_NAMESPACE_URL)
-				.addPrefix(NamespaceConstants.FEDORA_NAMESPACE_PREFIX, NamespaceConstants.FEDORA_NAMESPACE_URL)
-				.addVar("*")
-				.addWhere(subject, "dc:title", "?title")
-				.addWhere(subject, "fedora:created", "?created")
-				.addWhere(subject, "fedora:lastModified", "?lastModified")
-				.addOptional(subject, "ot:published", "?published")
-				.addOptional(subject, "ot:keywords", "?keywords")
-				.addOptional(subject, "ot:owner", "?owner")
-				.addOptional(subject, "ot:contributors", "?contributors")
-				.addOptional(subject, "ot:description", "?description")
-				.addOptional(subject, "ot:arkId", "?arkId")
-				.addOptional(subject, "ot:licenseName", "?licenseName")
-				.addOptional(subject, "ot:licenseLink", "?licenseLink");
+        return getFedoraObjects(query);
+      } else {
+        logger.error("Fuseki Server URL is not configured");
+        throw new LibraryException("Fuseki Server URL is not configured");
+      }
+    } catch (ConnectException ex) {
+      logger.error("Check if fuseki server up and running. " + ex);
+      throw new LibraryException(ex);
+    }
+  }
 
-		if(published) {
-			try {
-				getAllObjects.addFilter("?published = \"yes\" || ?published = true");
-			} catch(org.apache.jena.sparql.lang.sparql_11.ParseException e) {
-				logger.error("Invalid filter for published objects " + e);
-			}
-		}
-		return getAllObjects.build();
-	}
+  public int countUniqueArkIds() throws LibraryException {
+    try {
+      SelectBuilder countArkIds =
+          new SelectBuilder()
+              .addVar("count(*)", "?count")
+              .addWhere("?s", "<http://uofm.org/objectteller/arkId>", "?o");
+      QueryExecution queryExecution =
+          QueryExecutionFactory.sparqlService(fusekiServerURL, countArkIds.build());
+      ResultSet resultSet = queryExecution.execSelect();
+      return resultSet.next().get("count").asLiteral().getInt();
+    } catch (org.apache.jena.sparql.lang.sparql_11.ParseException e) {
+      throw new LibraryException("Cannot create query for count of ark ids" + e);
+    }
+  }
 
-	private ArrayList<KnowledgeObject> getFedoraObjects(Query query) throws ConnectException, LibraryException {
-		ArrayList< KnowledgeObject> list = new ArrayList<>();
-		QueryExecution execution = QueryExecutionFactory.sparqlService(fusekiServerURL, query);
+  private Query initQuery(boolean published) {
 
-		ResultSet resultSet;
-		try {
-			resultSet = execution.execSelect();
-		} catch (QueryExceptionHTTP e) {
-			throw new ConnectException("Cannot fetch object list from fuseki. " +  e);
-		}
+    String subject = "?x";
 
-		while (resultSet.hasNext()) {
-			QuerySolution binding = resultSet.nextSolution();
+    SelectBuilder getAllObjects =
+        new SelectBuilder()
+            .addPrefix(NamespaceConstants.OT_NAMESPACE_PREFIX, NamespaceConstants.OT_NAMESPACE_URL)
+            .addPrefix(NamespaceConstants.DC_NAMESPACE_PREFIX, NamespaceConstants.DC_NAMESPACE_URL)
+            .addPrefix(
+                NamespaceConstants.FEDORA_NAMESPACE_PREFIX, NamespaceConstants.FEDORA_NAMESPACE_URL)
+            .addVar("*")
+            .addWhere(subject, "dc:title", "?title")
+            .addWhere(subject, "fedora:created", "?created")
+            .addWhere(subject, "fedora:lastModified", "?lastModified")
+            .addOptional(subject, "ot:published", "?published")
+            .addOptional(subject, "ot:keywords", "?keywords")
+            .addOptional(subject, "ot:owner", "?owner")
+            .addOptional(subject, "ot:contributors", "?contributors")
+            .addOptional(subject, "ot:description", "?description")
+            .addOptional(subject, "ot:arkId", "?arkId")
+            .addOptional(subject, "ot:licenseName", "?licenseName")
+            .addOptional(subject, "ot:licenseLink", "?licenseLink");
 
-			KnowledgeObject knowledgeObject = null;
-			list.add(knowledgeObject);
-		}
+    if (published) {
+      try {
+        getAllObjects.addFilter("?published = \"yes\" || ?published = true");
+      } catch (org.apache.jena.sparql.lang.sparql_11.ParseException e) {
+        logger.error("Invalid filter for published objects " + e);
+      }
+    }
+    return getAllObjects.build();
+  }
 
-		return list;
-	}
-	
-	private Date convertRDFNodetoDate(RDFNode o) throws LibraryException {
-		Date convertedDate;
-		try {
-			String date = o.toString();
-			date = date.substring(0, 10);
-			convertedDate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(date);
-		} catch (ParseException e) {
-			logger.error("Unable to parse created on date or last updated date" + e.getCause());
-			throw new LibraryException("Unable to parse created on date or last updated date" + e.getCause(), e);
-		}
-		return convertedDate;
-	}
+  private ArrayList<KnowledgeObjectWrapper> getFedoraObjects(Query query)
+      throws ConnectException, LibraryException {
+    ArrayList<KnowledgeObjectWrapper> list = new ArrayList<>();
+    QueryExecution execution = QueryExecutionFactory.sparqlService(fusekiServerURL, query);
+
+    ResultSet resultSet;
+    try {
+      resultSet = execution.execSelect();
+    } catch (QueryExceptionHTTP e) {
+      throw new ConnectException("Cannot fetch object list from fuseki. " + e);
+    }
+
+    while (resultSet.hasNext()) {
+      QuerySolution binding = resultSet.nextSolution();
+
+      KnowledgeObjectWrapper knowledgeObject = null;
+      list.add(knowledgeObject);
+    }
+
+    return list;
+  }
+
+  private Date convertRDFNodetoDate(RDFNode o) throws LibraryException {
+    Date convertedDate;
+    try {
+      String date = o.toString();
+      date = date.substring(0, 10);
+      convertedDate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(date);
+    } catch (ParseException e) {
+      logger.error("Unable to parse created on date or last updated date" + e.getCause());
+      throw new LibraryException(
+          "Unable to parse created on date or last updated date" + e.getCause(), e);
+    }
+    return convertedDate;
+  }
 }
--- application/src/test/java/org/kgrid/library/services/IdServiceTest.java
+++ application/src/test/java/org/kgrid/library/services/IdServiceTest.java
@@ -1,94 +1,70 @@
 package org.kgrid.library.services;
 
-import org.kgrid.library.ezidGateway.DummyIdService;
-import org.kgrid.library.ezidGateway.EzidService;
-import java.net.URI;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.kgrid.library.ezidGateway.DummyIdService;
+import org.kgrid.library.ezidGateway.EzidService;
 import org.kgrid.shelf.domain.ArkId;
-import org.kgrid.shelf.domain.KnowledgeObject;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-
 public class IdServiceTest {
 
-	private static final String ARKID_STRING = "ark:/99999/fake";
+  private static final String ARKID_STRING = "ark:/99999/fake";
 
-	private IdService idService;
-	
-	@Mock
-	private EzidService ezidService;
+  private IdService idService;
 
-	@Mock
-	private DummyIdService dummyIdService;
-	
-	public IdServiceTest() {
-		MockitoAnnotations.initMocks(this);
-	}
-	
-	@Before
-	public void setUp() {
-		idService = new IdService(ezidService, dummyIdService);
-	}
+  @Mock private EzidService ezidService;
 
-	@After
-	public void tearDown() throws Exception {
+  @Mock private DummyIdService dummyIdService;
 
-	}
+  public IdServiceTest() {
+    MockitoAnnotations.initMocks(this);
+  }
 
-	@Test
-	public void testMethodDelegation() throws Exception {
-		when(ezidService.mint()).thenReturn(ARKID_STRING);
-		idService.mint();
-		verify(ezidService).mint();
-	}
+  @Before
+  public void setUp() {
+    idService = new IdService(ezidService, dummyIdService);
+  }
 
-	// TODO: reimplement ark id statuses
-	@Test
-	public void publishKnowledgeObject(){
+  @After
+  public void tearDown() throws Exception {}
 
-		ArrayList<String> metadata = new ArrayList<String>();
-		idService.publish(new ArkId("ark:/test/ark"), metadata);
-		
-//		verify(ezidService).status(ARKID_STRING, metadata, ArkId.Status.PUBLIC);
-	}
-	
-	@Test
-	public void retractId(){
-		ArrayList<String> metadata = new ArrayList<String>(); 
-		idService.retract(new ArkId(ARKID_STRING), metadata);
-		
-//		verify(ezidService).status(ARKID_STRING, metadata, ArkId.Status.UNAVAILABLE);
-	}
+  @Test
+  public void testMethodDelegation() throws Exception {
+    when(ezidService.mint()).thenReturn(ARKID_STRING);
+    idService.mint();
+    verify(ezidService).mint();
+  }
 
-	// TODO: Fix tests by setting ko to a real knowledge object
-	@Test
-	public void givenNewKoCanBindNewArkId() throws Exception {
+  // TODO: reimplement ark id statuses
+  @Test
+  public void publishKnowledgeObject() {
 
-		when(ezidService.mint()).thenReturn(ARKID_STRING);
+    ArrayList<String> metadata = new ArrayList<String>();
+    idService.publish(new ArkId("ark:/test/ark"), metadata);
 
-		KnowledgeObject ko = null;
-		
-		ArrayList<String> metadata = new ArrayList<String>();
+    //		verify(ezidService).status(ARKID_STRING, metadata, ArkId.Status.PUBLIC);
+  }
 
-//		idService.bind(ko.getArkId(), metadata, new URI("http://dev.umich.edu/"+ ARKID_STRING));
+  @Test
+  public void retractId() {
+    ArrayList<String> metadata = new ArrayList<String>();
+    idService.retract(new ArkId(ARKID_STRING), metadata);
 
-//		assertEquals(ARKID_STRING, ko.getArkId().toString());
+    //		verify(ezidService).status(ARKID_STRING, metadata, ArkId.Status.UNAVAILABLE);
+  }
 
-//		verify(ezidService).bind(ARKID_STRING, metadata, new URI("http://dev.umich.edu/"+ ARKID_STRING));
-
-	}
-
-	@Test
-	public void testPing() throws Exception {
-		when(ezidService.ping()).thenReturn(true);
-		assertEquals(true, idService.ping());
-	}
+  @Test
+  public void testPing() throws Exception {
+    when(ezidService.ping()).thenReturn(true);
+    assertEquals(true, idService.ping());
+  }
 }
